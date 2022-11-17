0a5c3d0637004ec7e760b2a0bc56ee840ce48114
Matiuribec <matiuribec2020@gmail.com>
2022-11-17 02:19:55 +0000

    LandingPage

---
+++ src/js/component/NavBar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const NavBar = () =>{
+
+
+    return (
+        
+            <nav className="navbar navbar-expand-lg bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Navbar</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+            </nav>
+
+
+    )
+}
+
+export default NavBar
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,31 @@
 import React from "react";
+import Card from './Card.jsx'
+import NavBar from './NavBar.jsx'
+import Jumbotron from './Jumbotron.jsx'
+
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+ 
+	const card =[
+		{img:{src:"https://picsum.photos/seed/1/150/150", alt:"Image"},btn:{href:"#", text:"Call to Action"},title:"Card Title",text:"A little text from here to there"},
+		{img:{src:"https://picsum.photos/seed/2/150/150", alt:"Image"},btn:{href:"#", text:"Call to Action"},title:"Card Title",text:"A little text from here to there"},
+		{img:{src:"https://picsum.photos/seed/3/150/150", alt:"Image"},btn:{href:"#", text:"Call to Action"},title:"Card Title",text:"A little text from here to there"},
+		{img:{src:"https://picsum.photos/seed/4/150/150", alt:"Image"},btn:{href:"#", text:"Call to Action"},title:"Card Title",text:"A little text from here to there"},
+
+	]
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<NavBar />
+			<Jumbotron />
+			<div className= "d-flex">
+				{card.map((card, index)=><Card key={index} button= {card.btn} img={card.img} title={card.title} text={card.text}/>)}
+			</div>
+			
 		</div>
 	);
 };
